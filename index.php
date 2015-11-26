b82b7fad8f163ead15ad3ee6994e6a9dbbc5fcff
Michael Stark <mike.stark@vidyard.com>
2015-11-25 21:15:26 -0500

    db

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
-
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+define('APP_DIR', 'app');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+?>
